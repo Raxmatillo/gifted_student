7d37e3f6de823de3fdfbcd223d957834068c6328
raxmatillo <milodcomposer@gmail.com>
2023-06-25 14:36:42 +0500

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomePageView, name='home'),
+    path('grants/', views.GrantListView, name='grants'),
+    path('grants/<int:pk>/', views.GrantDetailView, name='grant_detail'),
+    path('grants/form/', views.create_grant, name='create_grant'),
+
+    path('students/by_grant/<int:grant_id>/',
+         views.StudentListView, name='students'),
+    path('students/<int:pk>/', views.StudentDetailView, name='student_detail'),
+
+]
